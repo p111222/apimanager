907117fcd3c1cb085c06d324bf9ca5f45d58123c
p111222 <princejainpre.ram@gmail.com>
2025-05-19 17:25:26 +0530

    wow

--- src/Pages/AuthPages/Login.jsx
+++ src/Pages/AuthPages/Login.jsx
@@ -90,7 +90,7 @@ const Login = () => {
     <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
       <DotLottieReact
         src="https://lottie.host/daa52b92-4f61-46f3-b628-4e8763f992f0/uxk1wFWwXN.lottie"
-        className="w-[150px] h-[150px]"
+        className="w-[100px] h-[100px]"
         loop
         autoplay
       />
